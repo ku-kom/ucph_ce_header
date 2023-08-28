4ca1aa0bdfa59a5c9b1ce13f5c42ddcf8986b718
Nanna Ellegaard <nel@adm.ku.dk>
2023-08-28 12:19:57 +0200

    Rename

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -24,6 +24,48 @@ call_user_func(function ($extKey ='ucph_content_header', $contentType ='ucph_con
         'after'
     );
 
+    // Header styles config
+    
+    // Remove h1 and 'hidden'
+    // $GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items']['1'] = [''];
+    // $GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items']['6'] = [''];
+
+    // Rename header_layout label:
+    $GLOBALS['TCA']['tt_content']['columns']['header_layout']['label'] = 'LLL:EXT:ucph_content_header/Resources/Private/Language/locallang_be.xlf:ucph_content_header_type';
+
+    // Rename default label
+    $GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items']['0']['0'] = 'LLL:EXT:ucph_content_header/Resources/Private/Language/locallang_be.xlf:ucph_content_header_default';
+
+    // Rename h2 label
+    $GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items']['2']['0'] = 'H2';
+
+    // Rename h3 label
+    $GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items']['3']['0'] = 'H3';
+
+    // Rename h4 label
+    $GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items']['4']['0'] = 'H4';
+
+    // Rename h5 label
+    $GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items']['5']['0'] = 'H5';
+
+    // Add H6
+    $GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items'][] = [
+        'H6',
+        '10'
+    ];
+
+    // Add header divider
+    $GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items'][] = [
+        'LLL:EXT:ucph_content_header/Resources/Private/Language/locallang_be.xlf:ucph_content_header_devider',
+        '150'
+    ];
+
+    // Add hidden
+    $GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items'][] = [
+        'LLL:EXT:ucph_content_header/Resources/Private/Language/locallang_be.xlf:ucph_content_header_hidden',
+        '170'
+    ];
+
     // Add Content Element
     if (!is_array($GLOBALS['TCA']['tt_content']['types'][$contentType] ?? false)) {
         $GLOBALS['TCA']['tt_content']['types'][$contentType] = [];
